0ea8c5ef5b038ce92924e8ef27989ff94b622c83
eogus0512 <eogus0512@gmail.com>
2024-06-17 22:28:01 +0900

    env: 프로젝트 생성

---
+++ daehyeon/src/main/java/kfriday/subject/daehyeon/DaehyeonApplication.java
@@ -0,0 +1,13 @@
+package kfriday.subject.daehyeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaehyeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaehyeonApplication.class, args);
+	}
+
+}
---
+++ daehyeon/src/test/java/kfriday/subject/daehyeon/DaehyeonApplicationTests.java
@@ -0,0 +1,13 @@
+package kfriday.subject.daehyeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaehyeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
